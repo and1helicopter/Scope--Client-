9b99150b1be4668c1ad8669b5b6901118a133a94
and1helicopter <and1helicopter@mail.ru>
2016-10-03 11:58:00 +0300

    Соответсвующие изменения

--- Scope (Client)/ScopeSetupApp/ScopeSetupForm.Designer.cs
+++ Scope (Client)/ScopeSetupApp/ScopeSetupForm.Designer.cs
@@ -60,6 +60,7 @@
             this.possibleTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.sampleNameLabel = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.toolStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
@@ -130,6 +131,7 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.oscSize);
             this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.checkBox1);
@@ -440,6 +442,16 @@
             this.label2.Text = "Возможные для осциллографирования параметры";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(684, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 39);
+            this.button1.TabIndex = 11;
+            this.button1.TabStop = false;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // ScopeSetupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
@@ -512,6 +524,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox oscFreqRadioButton;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
 
         /*
         private System.Windows.Forms.RadioButton radioButton3;
